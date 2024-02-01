521a3d8d1b0248387bef7d7c7663821f268b768a
Aleksx07 <134466359+Aleksx07@users.noreply.github.com>
2024-02-01 13:23:24 -0600

    Agregar archivos de proyecto.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PM02PO12024
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ Views/PageInit.xaml.cs
@@ -0,0 +1,32 @@
+namespace PM02PO12024;
+
+public partial class PageInit : ContentPage
+{
+    Controllers.PersonasControllers PersonDB; 
+	public PageInit()
+	{
+		InitializeComponent();
+	}
+    public PageInit(Controllers.PersonasControllers dbpath)
+    {
+        InitializeComponent();
+        PersonDB = dbpath;
+    }
+
+    private async void btnprocesar_Clicked(object sender, EventArgs e)
+    {
+        var person = new Models.Personas
+        {
+            Nombres = nombres.Text,
+            Apellidos = apellidos.Text,
+            FechaNac = FechaNac.Date,
+            telefono = telefono.Text 
+
+        };
+
+        if (await PersonDB.StorePerson(person) > 0)
+        {
+            await DisplayAlert("Aviso", "Registro ingresado con exito!!", "OK"); 
+        }
+    }
+}
